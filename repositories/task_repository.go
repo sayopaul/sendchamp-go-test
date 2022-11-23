641eb727a5179bd3c31819a9e855b3bd570d482f
Sayo Paul <yobizzi416@gmail.com>
2022-11-23 03:22:06 +0100

    return updated task after updating a task

--- repositories/task_repository.go
+++ repositories/task_repository.go
@@ -23,8 +23,11 @@ func (tr TaskRepository) CreateTask(task models.Task) (models.Task, error) {
 	return task, tr.db.DB.Create(&task).Error
 }
 
-func (tr TaskRepository) UpdateTask(taskCondition models.Task, task models.Task) (models.Task, error) {
-	return task, tr.db.DB.Model(models.Task{}).Where(&taskCondition).Updates(&task).Error
+func (tr TaskRepository) UpdateTask(taskCondition models.Task, task models.Task) (updatedTask models.Task, err error) {
+	err = tr.db.DB.Model(models.Task{}).Where(&taskCondition).Updates(&task).Error
+	tr.db.DB.Model(&models.Task{}).Where(&task).First(&updatedTask)
+	return updatedTask, err
+
 }
 
 func (tr TaskRepository) DeleteTask(taskCondition models.Task) error {
