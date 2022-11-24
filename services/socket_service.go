0c9bc2760e5c4304d3d49fb5baeecc0b0c2e57d9
Sayo Paul <yobizzi416@gmail.com>
2022-11-24 15:42:16 +0100

    switched from native sockets to use gorilla websockets

--- services/socket_service.go
+++ services/socket_service.go
@@ -1,14 +1,15 @@
 package services
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
-	"net"
+	"net/url"
 	"os"
+	"os/signal"
 
+	"github.com/gorilla/websocket"
 	"github.com/sayopaul/sendchamp-go-test/config"
 )
 
@@ -22,31 +23,35 @@ func NewSocketService(configEnv config.Config) SocketService {
 	}
 }
 
-// Application constants, defining host, port, and protocol.
 func (ss SocketService) Send(message map[string]interface{}) {
-	// Start the client and connect to the server.
-	log.Println("Connecting to a ", ss.configEnv.SocketConnectionType, "server with hostname", ss.configEnv.SocketConnectionHost+":"+ss.configEnv.SocketConnectionPort)
-	//dial the server
-	conn, err := net.Dial(ss.configEnv.SocketConnectionType, ss.configEnv.SocketConnectionHost+":"+ss.configEnv.SocketConnectionPort)
+	var addr = flag.String("addr", fmt.Sprintf("%s:%s", ss.configEnv.SocketConnectionHost, ss.configEnv.SocketConnectionPort), "http service address")
+	flag.Parse()
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		fmt.Println("Error connecting:", err.Error())
-		os.Exit(1)
+		log.Panic("dial:", err.Error())
 	}
-	//close the connection when not in use
-	defer conn.Close()
-	//marshal to json
+	defer c.Close()
+
 	toJson, _ := json.Marshal(message)
-	byteMessage := bytes.NewReader(toJson)
-	reader := bufio.NewReader(byteMessage)
-	// run loop forever, until the econnection is exited
-	for {
-		// Read in input until newline, Enter key.
-		input, _ := reader.ReadString('\n')
-		//write message to connection
-		conn.Write([]byte(input))
-		// Listen for response from server
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// Print server response.
-		log.Print("The server responded with: " + message)
+	err = c.WriteMessage(websocket.TextMessage, []byte(toJson))
+	if err != nil {
+		log.Panic("error writing to the socket:", err.Error())
 	}
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read message error:", err)
+			return
+		}
+		log.Printf("recv: %s", message)
+	}
+
 }
